8e9136219268381df2d818e7f9ee76c070ffd7a4
Albert HK Cheng <albert.hk.cheng@gmail.com>
2018-07-26 14:44:24 +0800

    Add Tab Option for get

--- R/get.R
+++ R/get.R
@@ -3,11 +3,12 @@
 #' @seealso \href{https://developers.google.com/sheets/}{Google Documentation}
 #'
 #' @param data_type The expected data type of the output
+#' @param tabName The name of the spreadsheet tab
 #' @param spreadsheetId The ID of the spreadsheet to get
 #' @param range The A1 notation of the values to get
 #' @export
-spreadsheets_get <- function(data_type = "data.frame", spreadsheetId, range){
-  response <- spreadsheets.values.get(spreadsheetId, range)
+spreadsheets_get <- function(data_type = "data.frame", spreadsheetId, tabName = "", range = "A1"){
+  response <- spreadsheets.values.get(spreadsheetId, paste0(ifelse(tabName == "", "", paste0(tabName, "!")),range))
   if(data_type == "data.frame"){
     df <- as.data.frame(response$values[-1,])
     colnames(df) <- response$values[1,]
